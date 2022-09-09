4aa0276f29efd198f136892ebe20e7d65205ac6b
phoenixr-codes <phoenixr41414141@gmail.com>
2022-09-09 16:02:58 +0200

    edited docs structure

---
+++ docsrc/_build/html/_sources/minecraft-references.rst.txt
@@ -0,0 +1,28 @@
+********************
+Minecraft References
+********************
+
+================
+Target Selectors
+================
+
+.. list-table::
+   
+   * - **@s**
+     - entity executing the command
+   
+   * - **@p**
+     - nearest player
+   
+   * - **@a**
+     - all players
+   
+   * - **@e**
+     - all entities
+   
+   * - **@r**
+     - random player
+
+*... and a few more irrelevant ones ...*
+
+You may also just use the player's name.
---
+++ docsrc/conf.py
@@ -0,0 +1,65 @@
+#:==========================================
+# Sphinx Documentation Builder Configuration
+#:==========================================
+
+
+#########
+# Imports
+
+from datetime import date
+from importlib.metadata import version as version_of
+import os
+import sys
+
+
+#############
+# Change Path
+
+sys.path.insert(0, os.path.abspath("."))
+
+
+#####################
+# Project Information
+
+
+project = "bedrockpy"
+copyright = f"{date.today().year}, phoenixR"
+author = "phoenixR"
+release = version_of("bedrockpy")
+
+
+###############
+# Configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.napoleon",
+    
+    "sphinxcontrib_trio"
+]
+
+#nitpicky = True
+#pygments_style = "github-dark"
+
+
+#######################
+# Autodoc Configuration
+
+autodoc_typehints = "description"
+autoclass_content = "both"
+
+
+############
+# Find Files
+
+master_doc = "index"
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+###########################
+# HTML Output Configuration
+
+html_theme = "furo"
+html_static_path = ["_static"]
